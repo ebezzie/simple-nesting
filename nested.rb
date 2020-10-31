749eaae542d709e19c060550c5abf3f639403d4f
Ebony <ebony.furr@yahoo.com>
2020-10-30 21:32:07 -0500

    commit tree 287c23aca186e0761669b17e4c7f6b70f2b24ced

--- nested.rb
+++ nested.rb
@@ -15,13 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +52,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+    programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,7 +81,12 @@ def adding_matz
         }
      }
 
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+      known_for: "Ruby", 
+      languages: ["LISP", "C"]
+    }
+
+    return programmer_hash
 end
 
 def changing_alan
@@ -102,6 +108,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for]= "GUI"
+     return programmer_hash
      
 end
 
@@ -121,5 +129,12 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     new_language = programmer_hash[:dennis_ritchie]
+     new_language2 = new_language[:languages]
+     new_language2 << "Assembly"
+
+     return programmer_hash
+     
+     
 
 end
